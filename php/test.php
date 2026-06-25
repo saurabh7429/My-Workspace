ac7fe5ef51849620010630ff56b47c1accb03765
Saurabh <saurabh7429>
2026-06-25 19:01:29 +0530

    Add PHP setup confirmation script

---
+++ php/test.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h1>Ubuntu 24.04 LTS par PHP Setup Successful!</h1>";
+echo "<p>Saurabh, aapka workspace folder proper link ho gaya hai.</p>";
+?>
+git config --global user.email "saurabh7429"
+git config --global user.name "Saurabh"
