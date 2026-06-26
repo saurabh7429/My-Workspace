67a3065a932a157d723d9ba9c850246ba586e3ef
Saurabh <m.saurya1600x@gmail.com>
2026-06-26 20:21:51 +0530

    Remove unnecessary git configuration commands from test.php and add a new comment.php file

---
+++ php/comment.php
@@ -0,0 +1,7 @@
+<?php //this is php initializing tag
+    # now we write single line in echo
+    echo "hello<br>";
+    /* now we write
+    multi line code in echo statement */
+    echo "This is php code<br>Written in multiline";    
+?>
--- php/test.php
+++ php/test.php
@@ -2,5 +2,3 @@
 echo "<h1>Ubuntu 24.04 LTS par PHP Setup Successful!</h1>";
 echo "<p>Saurabh, aapka workspace folder proper link ho gaya hai.</p>";
 ?>
-git config --global user.email "saurabh7429"
-git config --global user.name "Saurabh"
