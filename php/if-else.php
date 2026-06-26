8db19ed9f4e21bb9dd6eaa946a4d5a7abc849646
Saurabh <m.saurya1600x@gmail.com>
2026-06-26 20:33:36 +0530

    Add if-else.php to demonstrate conditional statements in PHP

---
+++ php/if-else.php
@@ -0,0 +1,12 @@
+<?php
+    $marks = 20;
+    $name = "Saurabh";
+    
+    if ($marks >= 35){
+        echo "Result: pass<br>";
+        echo $name . ", Congratulations! You Passed.<br>";
+    } else {
+        echo "Result: Fail<br>";
+        echo $name . ", Better luck next time.<br>";
+    }
+?>
