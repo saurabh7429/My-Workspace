d44c9777ec8f443552821a1df2fd98830ead0fcf
Saurabh <m.saurya1600x@gmail.com>
2026-06-26 20:21:51 +0530

    Add variable.php to display user information

--- php/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-    echo "hello <br>";
-    echo "my name is saurabh <br>";
-    echo "i am learning php <br>";
-?>
---
+++ php/variable.php
@@ -0,0 +1,8 @@
+<?php
+    $name = "Saurabh";
+    $age = 20;
+    $college = "Udhna Citizen College";
+    echo "name: ", $name, "<br>";
+    echo "age: ", $age, "<br>";
+    echo "College: ", $college, "<br>";
+?>
