3c680b3c6d0531909fe3a23313b3ba1666ed3dfd
Saurabh <m.saurya1600x@gmail.com>
2026-06-26 20:21:51 +0530

    Add datatypes.php to demonstrate variable types in PHP

---
+++ php/datatypes.php
@@ -0,0 +1,14 @@
+<?php
+    $name = "Saurabh"; 
+        // String
+    $age = 20;
+        // Integer
+    $cgpa = 8.36;
+        // Float
+    $isStudent = true;
+        // Boolean
+    echo "Name: ". $name . "<br>";
+    echo "Age: " . $age . "<br>";
+    echo "CGPA: " . $cgpa . "<br>";
+    echo "IsStudent: " . $isStudent . "<br>";
+?>
