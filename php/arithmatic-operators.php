770b3e09e68cd1b12341006587c058e44b75321c
Saurabh <m.saurya1600x@gmail.com>
2026-06-26 20:21:51 +0530

    Add arithmatic-operators.php to demonstrate basic arithmetic operations in PHP

---
+++ php/arithmatic-operators.php
@@ -0,0 +1,10 @@
+<?php
+    $num1 = 25;
+    $num2 = 7;
+
+    echo "Addition: " . ($num1 + $num2) . "<br>"; 
+    echo "Subtraction: " . ($num1 - $num2) . "<br>";
+    echo "Multiplication: " . ($num1 * $num2) . "<br>";
+    echo "Division: " . ($num1 / $num2) . "<br>";
+    echo "Remainder: " . ($num1 % $num2) . "<br>";
+?>
