a3d59974415a0682ab7f72c8543c518b7d800356
Saurabh <m.saurya1600x@gmail.com>
2026-06-26 20:21:51 +0530

    Add comparision-operator.php to demonstrate comparison operations in PHP

---
+++ php/comparision-operator.php
@@ -0,0 +1,16 @@
+<?php
+    $age = 20;
+    $requiredAge = 18;
+    var_dump($age == $requiredAge); 
+    echo "<br>";
+    var_dump($age != $requiredAge);
+    echo "<br>";
+    var_dump($age<$requiredAge);
+    echo "<br>";
+    var_dump($age>$requiredAge);
+    echo "<br>";
+    var_dump($age<=$requiredAge);
+    echo "<br>";
+    var_dump($age>=$requiredAge);
+    echo "<br>";
+?>
