d598be9ca69f2396bc81801b68becf5b6c8f3ebc
Saurabh <m.saurya1600x@gmail.com>
2026-06-27 16:35:04 +0530

    Add first-html.php to display student information using PHP variables

---
+++ php/first-html.php
@@ -0,0 +1,18 @@
+<?php
+    $name = 'saurabh';
+    $age = 20;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Student Information</h1>
+    <p>Name: <?php echo $name; ?></p>
+    <p>Age: <?php echo $age; ?></p>
+</body>
+</html>
