b8bfbeddbdd3ce1243758be6410bc4350f17e92a
Saurabh <m.saurya1600x@gmail.com>
2026-07-01 22:27:14 +0530

    arrays

---
+++ php/array.php
@@ -0,0 +1,13 @@
+<?php 
+    $students = [
+        "Rahul",
+        "Aman",
+        "Saurabh",
+        "Priya",
+        "Rohit"
+    ];
+
+    echo "First Student: " . $students[0];
+    echo "<br>Last Student: " . $students[4];
+    echo "<br>total Students: " . count($students); 
+?>
