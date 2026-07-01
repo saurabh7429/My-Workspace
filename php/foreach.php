653b486de0f0360ae9a0638f84051bbf50d57bd2
Saurabh <m.saurya1600x@gmail.com>
2026-07-01 22:31:36 +0530

    Add foreach loop to display student names

---
+++ php/foreach.php
@@ -0,0 +1,13 @@
+<?php 
+    $students = [
+        "Rahul",
+        "Aman",
+        "Saurabh",
+        "Priya",
+        "Rohit"
+    ];
+
+    foreach( $students as $stud ) {
+        echo $stud . "<br>";
+    }
+?>
