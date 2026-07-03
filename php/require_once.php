7f786791a4358d5423ecb027b99f79244fb8cb49
Saurabh <m.saurya1600x@gmail.com>
2026-07-03 20:50:19 +0530

    created doc that shows use of include and require

---
+++ php/require_once.php
@@ -0,0 +1,11 @@
+// this document shows you how to use of require and include functions in php
+
+/*
+| Statement      | File na mile to                 | 2 baar likho to          |
+| -------------- | ------------------------------- | ------------------------ |
+| `include`      | Warning, program chalta rahega  | 2 baar include hogi      |
+| `require`      | Fatal Error, program ruk jayega | 2 baar include hogi      |
+| `include_once` | Warning                         | Sirf 1 baar include hogi |
+| `require_once` | Fatal Error                     | Sirf 1 baar include hogi |
+
+*/
