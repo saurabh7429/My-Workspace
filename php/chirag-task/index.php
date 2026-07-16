92d3222a74c3f6999ec244c28f773c1af51f10d4
Saurabh <m.saurya1600x@gmail.com>
2026-07-16 17:14:44 +0530

    Add initial HTML structure for home page with form elements

---
+++ php/chirag-task/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+</head>
+<body>
+    <h3>Home page</h3>
+
+    <form action="" method="get">
+
+        <label for="name">Name :</label>
+        <input type="text" name="name" require>
+
+        <button type="submit">Submit</button>
+    </form>
+
+    <?php 
+        $emp = [
+                "id" => [1],
+                "name" => ["saurabh"]
+            ];
+    ?>
+</body>
+</html>
