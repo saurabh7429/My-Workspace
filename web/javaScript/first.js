afcf946ebe190be982af64bf450bf78475626ec7
Saurabh <m.saurya1600x@gmail.com>
2026-07-23 16:27:10 +0530

    Add user profile object with details and interaction buttons

--- web/javaScript/first.js
+++ web/javaScript/first.js
@@ -1,2 +1,10 @@
-name = "saurabh";
-console.log(name);
+const profile = {
+    username: "saurabh7429",
+    postCount: 195,
+    followers: 596,
+    following: 4,
+    name: "Saurabh Maurya",
+    bio: "i'm  bca ty student, who love to learn new intresting things.",
+    tag: "student",
+    buttons : ["follow", "message"]
+}
