043c0b5f0efd23ee694fbcf87f0eea5d9bba76df
Saurabh <m.saurya1600x@gmail.com>
2026-07-24 19:28:47 +0530

    Update index.html to reference loop.js and add loop.js with various loop examples

---
+++ web/javaScript/loop.js
@@ -0,0 +1,40 @@
+// // for loop
+// for (let i = 0; i <=5; i++) {
+//     console.log("i = " , i);
+// }
+
+
+
+// // while loop
+// let i =1;
+// while(i<=100) {
+//     if (i%2==0)
+//         console.log( i, " is even");
+//     else
+//         console.log( i, " is odd");
+//     i++;
+// }
+
+// // do while
+// let i=5
+// do {
+//     console.log("i = ", i);
+//     i--;
+// } while(i);
+
+// // for of loop
+// let str = "abcdefghijklmnopqrstuvwxyz";
+// for(let i of str){
+//     console.log("i = ", i);
+// }
+
+// // for in loop 
+// let student = {
+//     id: 1,
+//     name: "saurabh",
+//     age: 25
+// };
+// for(let i in student) {
+//     console.log("i= ", i);
+//     console.log("key = ", i, ", value = ", student[i]);
+// }
